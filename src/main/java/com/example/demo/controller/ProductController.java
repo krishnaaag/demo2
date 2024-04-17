a67089577faa8bdc3085b7689367cec379166365
krishnaaag <krishnaagrawal1990@gmail.com>
2024-04-18 00:05:15 +0530

    Implement product service

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import com.example.demo.models.Product;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/product")
+        public class ProductController {
+
+    // https://fakestoreapi.com/products
+
+    @GetMapping("/{id}/{temp}")
+    public String getProductById(@PathVariable ("id") Long id, @PathVariable("temp") String x)
+    {
+        return "Here's your product "+ id+x;
+    }
+    @GetMapping("/{id}")
+    public String getProductById(@PathVariable ("id") Long id)
+    {
+        return "Here's your Product " + id;
+    }
+    @PostMapping()
+    public String createProduct(@RequestBody Product product)
+    {
+        return "Product Created";
+    }
+}
---
+++ src/main/java/com/example/demo/models/Product.java
@@ -0,0 +1,11 @@
+package com.example.demo.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    private String name;
+    private int price;
+}
